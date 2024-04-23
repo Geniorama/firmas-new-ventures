eb3aa6f9ff2ff03a0e2d7a3395f4daac23bb4c41
angelpublicista <angelpublicista.1@gmail.com>
2024-04-23 17:28:46 -0500

    feat: finish functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,55 @@
-const form = document.getElementById('form-nvc');
-console.log('main cargado')
+const form = document.getElementById("form-nvc");
 
-if(form){
-    const inputs = document.querySelectorAll('input:not([type*="submit"])')
+new ClipboardJS('#btn-copy');
 
-    inputs.forEach((el) => {
-        const target = el.dataset.target
+if (form) {
+  const inputs = document.querySelectorAll('input:not([type*="submit"])');
+  const tel = document.getElementById("intl-phone");
 
-        if(target){
-            el.addEventListener('input', function(){
-                const text = document.querySelector(`${target}`)
-                text.innerText = el.value
-            })
+  const iti = window.intlTelInput(tel, {
+    // any initialisation options go here
+    utilsScript: "./libs/intl-tel-input/js/utils.js",
+    customPlaceholder: function (
+      selectedCountryPlaceholder,
+      selectedCountryData
+    ) {
+      return "Ej: " + selectedCountryPlaceholder;
+    },
+    initialCountry: "mx",
+  });
+
+  function getPhoneNumber() {
+    var phoneNumber = iti.getNumber(); // Obtiene el número de teléfono completo
+    var countryData = iti.getSelectedCountryData(); // Obtiene los datos del país seleccionado
+    var countryCode = countryData.dialCode; // Obtiene el código de país
+    return {
+      phoneNumber,
+      countryData,
+      countryCode,
+    };
+  }
+
+  tel.addEventListener("countrychange", function () {
+    let phone = getPhoneNumber();
+    document.querySelector("#text-telefono .country-code").innerText =
+      "+" + phone.countryCode;
+  });
+
+  inputs.forEach((el) => {
+    const target = el.dataset.target;
+
+    if (target) {
+      el.addEventListener("input", function () {
+        const text = document.querySelector(`${target}`);
+
+        if (el.getAttribute("type") == "tel") {
+          const number = text.querySelector(".number");
+          number.innerText = el.value;
+          return;
         }
-    })
+
+        text.innerText = el.value;
+      });
+    }
+  });
 }
